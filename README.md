5035f1b14537c4f3f48fbd44c0d2b688711228a3
Ralf S. Engelschall <rse@engelschall.com>
2016-04-23 23:58:38 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# syspath
-System Paths Determination (Home and Data Directories)
+
+SysPath
+=======
+
+**System Paths Determination (Home and Data Directories)**
+
+<p/>
+<img src="https://nodei.co/npm/syspath.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/syspath.png" alt=""/>
+
+Abstract
+--------
+
+This is a small [Node](https://nodejs.org/) module for conveniently
+determining the home and data directories of an application.
+
+Installation
+------------
+
+```sh
+$ npm install syspath
+```
+
+Usage
+-----
+
+```js
+import syspath from "syspath"
+let { homeDir, dataDir } = syspath({ appName: "foo" })
+console.log(`homeDir: ${homeDir}, dataDir: ${dataDir}`)
+```
+
+Application Programming Interface
+---------------------------------
+
+```ts
+syspath({
+    appName?:           string,
+    dataDirMode?:       number,
+    dataDirAutoCreate?: boolean,
+    dataDirAutoRemove?: boolean,
+}): {
+    homeDir:            string,
+    dataDir:            string
+}``
+
+The `appName` is the name of the application. By default this is
+`path.basename(process.argv[1]).replace(/\.[^.]+$/, "")`.
+
+The `dataDirMode` is the numerical filesystem mode used for creating the
+`dataDir` (if still not existing and `dataDirAutoCreate` is `true`). The
+default is `parseInt("0755", 8) & ~process.umask()`.
+
+The `dataDirAutoCreate` forces the automatic creation of the `dataDir`
+(if still not existing, which is usually the case on the initial usage).
+By default this is `true`.
+
+The `dataDirAutoRemove` forces the automatic removal of the `dataDir` if
+it is still empty at the application termination time. By default this
+is `true`.
+
+The `homeDir` is the resolved path to the home directory of the user
+the application is executed under. The home directory is usually
+`%USERPROFILE%` on Windows or `$HOME` on Mac OS X, Linux and FreeBSD.
+
+The `dataDir` is the resolved path to the dedicated data directory for
+the application. The data directory is usually `%APPDATA%/<appName>` on
+Windows, `$HOME/Library/Application Support/<appName>` on Mac OS X and
+`$XDG_CONFIG_HOME/<appName>` or `$HOME/.config/<appName>` on Linux and
+FreeBSD.
+
+License
+-------
+
+Copyright (c) 2016 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ syspath.js
@@ -0,0 +1,85 @@
+/*!
+**  SysPath -- System Paths Determination (Home and Data Directories)
+**  Copyright (c) 2016 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  internal requirements  */
+var os     = require("os");
+var fs     = require("fs");
+var path   = require("path");
+
+/*  external requirements  */
+var mkdirp = require("mkdirp");
+
+/*  the API function  */
+module.exports = function (opts) {
+    /*  determine options  */
+    var options = {
+        appName:           path.basename(process.argv[1]).replace(/\.[^.]+$/, ""),
+        dataDirMode:       (parseInt("0755", 8) & ~process.umask()),
+        dataDirAutoCreate: true,
+        dataDirAutoRemove: true
+    };
+    if (typeof opts === "object")
+        Object.assign(options, opts);
+
+    /*  determine home directory  */
+    var homeDir = "";
+    if (process.env.USERPROFILE)
+        homeDir = path.resolve(process.env.USERPROFILE);
+    else if (process.env.HOME)
+        homeDir = path.resolve(process.env.HOME);
+    else
+        homeDir = path.resolve(os.homedir());
+    if (!fs.existsSync(homeDir))
+       throw new Error("home directory not found: \"" + homeDir + "\"");
+
+    /*  determine data directory  */
+    var dataDir = "";
+    var platform = os.platform();
+    if (platform === "win32")
+        dataDir = process.env.APPDATA;
+    else if (platform === "darwin")
+        dataDir = path.join(homeDir, "Library", "Application Support");
+    else if (process.env.XDG_CONFIG_HOME)
+        dataDir = path.resolve(process.env.XDG_CONFIG_HOME);
+    else
+        dataDir = path.join(homeDir, ".config");
+    dataDir = path.join(dataDir, options.appName);
+    if (!fs.existsSync(dataDir)) {
+        if (options.dataDirAutoCreate) {
+            mkdirp.sync(dataDir, { mode: options.dataDirMode });
+            if (!fs.existsSync(dataDir))
+                throw new Error("failed to auto-create data directory: \"" + dataDir + "\"");
+            if (options.dataDirAutoRemove)
+                process.on("exit", function (code) {
+                    try { fs.rmdirSync(dataDir); } catch (ex) {}
+                });
+        }
+        else
+            throw new Error("data directory not found: \"" + dataDir + "\"");
+    }
+
+    /*  return home and data directory  */
+    return { homeDir: homeDir, dataDir: dataDir };
+};
+
